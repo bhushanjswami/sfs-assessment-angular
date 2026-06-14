79bd23263e3841bb91c937cd602d8603ddd4db52
Bhushan <bhushanjswami@google.com>
2026-06-14 17:15:29 +0530

    created interface for interrupt and order

---
+++ src/models/interrupt
@@ -0,0 +1,5 @@
+export interface Interrupt {
+  time: Date;
+  reason: string;
+  status: string;
+}
---
+++ src/models/order.ts
@@ -0,0 +1,5 @@
+export interface Order {
+  orderNumber: string;
+  productionTarget:string;
+  productionState: string;
+}
