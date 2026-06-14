9d6eb00867ea31a740e102a050ae36b7706dd823
Bhushan <bhushanjswami@google.com>
2026-06-14 17:17:40 +0530

    created service for event API to get data

--- src/app/services/event.service.ts
+++ src/app/services/event.service.ts
@@ -1,21 +1,26 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { ConfigService } from './config.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EventService {
-configService: any;
+  constructor(private configService:ConfigService){}
 connect(deviceId: string): Observable<any> {
 
     return new Observable(observer => {
       //const endpoint = this.configService.getEndpoint('events');
      // console.log('Connecting to event source with endpoint:', endpoint, 'and deviceId:', deviceId);
-      const source = new EventSource(
-        `https://mock-api.assessment.sfsdm.org/events/${deviceId}`
-      );
-      console.log('Connecting to event source at:', `https://mock-api.assessment.sfsdm.org/events/${deviceId}`);
+     const endpoint = this.configService.getEndpoint('events'); // Returns: "/order/{orderId}"
+  
+  // Replace the placeholder {orderId} with the actual ID
+  const url = `${environment.apiBaseUrl}${endpoint.replace('{deviceId}', deviceId)}`;
+        console.log('Connecting to event source at:', url);
+
+      const source = new EventSource(url);
+      console.log('Connecting to event source at:', url);
       source.onmessage = event => {
 
         observer.next(
