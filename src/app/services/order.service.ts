368a46eed222878a4a13aa28455b541114520b51
Bhushan <bhushanjswami@google.com>
2026-06-15 20:19:09 +0530

    removed console log and unwanted comments

--- src/app/services/order.service.ts
+++ src/app/services/order.service.ts
@@ -2,22 +2,20 @@ import { Injectable } from '@angular/core';
 import { ConfigService } from './config.service';
 import { environment } from 'src/environments/environment';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import {Order} from '../../models/order'
 @Injectable({
   providedIn: 'root'
 })
 export class OrderService {
 
-  constructor(private configService: ConfigService,private http: HttpClient) { }
+  constructor(private configService: ConfigService, private http: HttpClient) { }
 
-getOrder(orderId: string) {
-  const endpoint = this.configService.getEndpoint('order'); // Returns: "/order/{orderId}"
-  
-  // Replace the placeholder {orderId} with the actual ID
-  const url = `${environment.apiBaseUrl}${endpoint.replace('{orderId}', orderId)}`;
+  // Method to fetch data from order API
+  getOrder(orderId: string) {
+    const endpoint = this.configService.getEndpoint('order');
 
-  return this.http.get(url);
-}
+    const url = `${environment.apiBaseUrl}${endpoint.replace('{orderId}', orderId)}`;
+
+    return this.http.get(url);
+  }
 
 }
